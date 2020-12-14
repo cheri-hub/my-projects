ee86c276d82e2c86d6199921e5663b5fcb87b145
Luis Montagner <63523907+winesherrie@users.noreply.github.com>
2020-12-14 12:45:44 -0300

    Add files via upload

---
+++ Portfolio/beforeUpdate/jumpy-bunny/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1605364734,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/ground_stone-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/gold_-sheet0.png",
+		"images/gameover-sheet0.png",
+		"images/joysticksplitted-sheet0.png",
+		"images/smallhandlefilled-sheet0.png",
+		"images/joystick-sheet0.png",
+		"images/buttonpstrianglewhite-sheet0.png",
+		"images/buttonpstrianglewhite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
---
+++ Portfolio/beforeUpdate/script.js
@@ -0,0 +1,41 @@
+$(window).scroll(function() {
+
+    var position = $(this).scrollTop(); //Representa a posição atual da tela, digamos assim.
+    
+    $($('.wrapper').children()).each(function() { //Essa função passa por todas as seções criadas na div principal, a .wrapper.
+        var target = $(this).offset().top; //Variável que contém o valor da posição onde uma seção começa (lembre-se que ela passa por todas)
+        var id = $(this).attr('id'); //Variável que pega o id dessa seção.
+
+        if (position >= target) { //Caso a posição atual seja maior ou igual à posição de uma das divs, quer dizer que você está nesta div em questão.
+            var alvo; //Variável para pegar o ID da seção no menu.
+            if(id == 'homepage'){ //Série de ifs para fazer a transferência de nome da seção para nome no menu. (pode ser otimizado mudando os nomes dos id's do menu)
+                alvo = '#h';
+            }
+            if(id == 'services'){
+                alvo = '#s';
+            }
+            if(id == 'projects'){
+                alvo = '#p';
+            }
+            if(id == 'about'){
+                alvo = '#a';
+            }
+            if(id == 'contact'){
+                alvo = '#c';
+            }
+
+            $('#menu > li').removeClass('active'); //Remove a classe .activated de todos <li>
+            $(alvo).addClass('active');//Adiciona a classe .activated no <li> da seção que você se encontra.
+        }
+
+    });
+});
+
+function hamburguer() {
+    var hamenu = document.getElementById("hamenu");
+    if (hamenu.className === "ham") {
+      hamenu.className = "on";
+    } else {
+      hamenu.className = "ham";
+    }
+  }
